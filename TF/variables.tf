edfc1cfc5df791e6eb6c9bb6b959763c9ad643e6
Vlad Klimenchenko <klimenchenko.vlad18@gmail.com>
2025-08-30 13:44:30 +0000

    Update repo

--- TF/variables.tf
+++ TF/variables.tf
@@ -6,11 +6,29 @@ variable "GOOGLE_PROJECT" {
 variable "GOOGLE_REGION" {
   type        = string
   default     = "us-central1-c"
-  description = "GCP region to use"
+  description = "GCP region"
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "g1-small"
+  description = "Machine type"
 }
 
 variable "GKE_NUM_NODES" {
   type        = number
   default     = 2
-  description = "GKE nodes number"
+  description = "Number of nodes"
+}
+
+variable "GKE_CLUSTER_NAME" {
+  type        = string
+  default     = "main"
+  description = "Cluster name"
+}
+
+variable "GKE_POOL_NAME" {
+  type        = string
+  default     = "main"
+  description = "Node pool name"
 }
--- TF/vars.tfvars
+++ TF/vars.tfvars
@@ -1,3 +1,6 @@
-GOOGLE_PROJECT = "vlad-k8s-demo-001"
-GOOGLE_REGION  = "us-central1-c"
-GKE_NUM_NODES  = 2
+GOOGLE_PROJECT   = "my-gcp-project-id"
+GOOGLE_REGION    = "us-central1-c"
+GKE_NUM_NODES    = 2
+GKE_MACHINE_TYPE = "e2-medium"
+GKE_CLUSTER_NAME = "main"
+GKE_POOL_NAME    = "main"
