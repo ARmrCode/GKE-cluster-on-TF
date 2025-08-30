41cd2f84b08051ab5bd3a248fd50d54230d6589f
armrcode <98815717+ARmrCode@users.noreply.github.com>
2025-08-30 16:28:45 +0300

    Update main.tf

--- TF/main.tf
+++ TF/main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "github.com/<ВАШ-РЕПОЗИТОРІЙ>/tf-google-gke-cluster"
+  source         = "git::https://github.com/ARmrCode/GKE-cluster-on-TF.git//TF"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
